7a0e25f692af36ceee538ccd16b178687e050cd4
aafnx <aafnx@proton.me>
2022-12-28 22:23:11 +0300

    init lesson-3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,6 @@
 package src;
-//1. Реализуйте метод, который запрашивает у пользователя ввод дробного числа
-// (типа float), и возвращает введенное значение.
-// Ввод текста вместо числа не должно приводить к падению приложения,
-// вместо этого, необходимо повторно запросить у пользователя ввод данных.
-//
-// 2. Если необходимо, исправьте данный код
-// (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-
-// 3. Дан следующий код, исправьте его там, где требуется
-// (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-//
-// 4. Разработайте программу, которая выбросит Exception,
-// когда пользователь вводит пустую строку.
-// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        // задание 1
-        System.out.println(Task1.getFloat());
-        // задание 2
-        Task2.init();
-        // задание 3
-        Task3.init();
-        // задание 4
-        Task4.init();
+        System.out.println("Hello World!");
     }
 }
